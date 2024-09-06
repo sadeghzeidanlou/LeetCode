e828cb7ebc1693bad4134510ad403118841a63d6
Sadegh Zeidanlou <46355431+sadeghzeidanlou@users.noreply.github.com>
2024-09-06 23:20:12 +0330

    Add project files.

---
+++ LeetCode75.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+
+namespace LeetCode
+{
+    public class LeetCode75
+    {
+        public static string MergeStringsAlternately(string word1, string word2)
+        {
+            if (string.IsNullOrEmpty(word1) && string.IsNullOrEmpty(word2))
+                return string.Empty;
+
+            if (string.IsNullOrEmpty(word1))
+                return word2;
+
+            if (string.IsNullOrEmpty(word1))
+                return word2;
+
+            var sb = new StringBuilder();
+            if (word1.Length <= word2.Length)
+            {
+                for (int i = 0; i < word1.Length; i++)
+                {
+                    sb.Append(word1[i].ToString() + word2[i].ToString());
+                }
+                if(word1.Length == word2.Length)
+                    return sb.ToString();
+
+                sb.Append(word2.Substring(word1.Length));
+                return sb.ToString();
+            }
+            for (int i = 0; i < word2.Length; i++)
+            {
+                sb.Append(word1[i].ToString() + word2[i].ToString());
+            }
+            sb.Append(word1.Substring(word2.Length));
+            return sb.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mergeAlternatelyResult = LeetCode75.MergeStringsAlternately("abc", "pqr");
+             mergeAlternatelyResult = LeetCode75.MergeStringsAlternately("ab", "pqrs");
+             mergeAlternatelyResult = LeetCode75.MergeStringsAlternately("abcd", "pq");
+        }
+    }
+}
