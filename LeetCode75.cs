5021be542ae8062bd105f88f851a2be554dd67d0
Sadegh Zeidanlou <46355431+sadeghzeidanlou@users.noreply.github.com>
2024-09-07 22:21:02 +0330

    Complete Problem 1

--- LeetCode75.cs
+++ LeetCode75.cs
@@ -6,22 +6,12 @@ namespace LeetCode
     {
         public static string MergeStringsAlternately(string word1, string word2)
         {
-            if (string.IsNullOrEmpty(word1) && string.IsNullOrEmpty(word2))
-                return string.Empty;
-
-            if (string.IsNullOrEmpty(word1))
-                return word2;
-
-            if (string.IsNullOrEmpty(word1))
-                return word2;
-
             var sb = new StringBuilder();
             if (word1.Length <= word2.Length)
             {
                 for (int i = 0; i < word1.Length; i++)
-                {
                     sb.Append(word1[i].ToString() + word2[i].ToString());
-                }
+                
                 if(word1.Length == word2.Length)
                     return sb.ToString();
 
@@ -29,11 +19,15 @@ namespace LeetCode
                 return sb.ToString();
             }
             for (int i = 0; i < word2.Length; i++)
-            {
                 sb.Append(word1[i].ToString() + word2[i].ToString());
-            }
+            
             sb.Append(word1.Substring(word2.Length));
             return sb.ToString();
+        }
+
+        public string GcdOfStrings(string str1, string str2)
+        {
+
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
-﻿namespace LeetCode
+﻿using System.Diagnostics;
+
+namespace LeetCode
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var mergeAlternatelyResult = LeetCode75.MergeStringsAlternately("abc", "pqr");
-             mergeAlternatelyResult = LeetCode75.MergeStringsAlternately("ab", "pqrs");
-             mergeAlternatelyResult = LeetCode75.MergeStringsAlternately("abcd", "pq");
+            var sw = new Stopwatch();
+            sw.Start();
+            var mergeAlternatelyResult = LeetCode75.MergeStringsAlternately("abcd", "pq");
+            sw.Stop();
         }
     }
 }
